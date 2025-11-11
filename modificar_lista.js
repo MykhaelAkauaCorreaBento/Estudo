29d81a93eef111bceffb60c82871de4ce1067947
MykhaelAkauaCorreaBento <mykhael.akaua.correa.bento@gmail.com>
2025-11-11 11:20:39 -0300

    Exercícios

---
+++ modificar_lista.js
@@ -0,0 +1,4 @@
+var linguagens = ["JavaScript", "PHP", "JAVA", "C#"];
+linguagens[2] = "C";
+
+console.log(linguagens);
---
+++ quantidade_de_elementros.js
@@ -0,0 +1,5 @@
+var linguagens = ["JavaScript", "PHP", "JAVA", "C#"];
+
+console.log(linguagens.length);
+
+// para saber quantos elementos tem dentro do arry
