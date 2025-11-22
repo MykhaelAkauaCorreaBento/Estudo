f3b31bccaa01e482fee782f8e22c4efe8a3d056e
MykhaelAkauaCorreaBento <mykhael.akaua.correa.bento@gmail.com>
2025-11-22 14:07:11 -0300

    Projetos
    Fim um jogo da forca e alguns exercícios.

---
+++ Projetos/Construa-um-Jogo-de-forca/dados/dados.js
@@ -0,0 +1,14 @@
+//dados.js
+
+
+const listaDeFrutas = [
+    "Pera","Banana", "Laranja","Abacaxi",
+    "Manga", "Uva", "Abacate", "Jaca", "Caju",
+    "Melancia", "Vergamota", "Kiwi", "Goiaba",
+    "Cereja", "Morango", "Maracuja", "Pessego",
+    "Pitanga", "Cacu", "Manga", "Amora", "Caqui",
+    "Figo", "Framboesa", "ameixa", "Butia", "Coco",
+    "Limao", "Maça", "Melao", "Roma", "Pitaia", "Pinha"
+];
+
+export default listaDeFrutas;
