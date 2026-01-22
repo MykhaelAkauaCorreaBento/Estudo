c80876c80921a1d3510fbd69e4faa3fb7f4a19be
MykhaelAkauaCorreaBento <mykhael.akaua.correa.bento@gmail.com>
2026-01-22 16:42:43 -0300

    Estudos

--- React-Framework/Projetos/react/projeto-sorveteria/src/componentes/Rodape/index.jsx
+++ React-Framework/Projetos/react/projeto-sorveteria/src/componentes/Rodape/index.jsx
@@ -1,39 +1,44 @@
 import Image from "next/image";
 import estilos from "./Rodape.module.css";
 
-export default function Rodape(){
-    return(
-        <footer>
-        <section>
-            <div>
-            <Image src="/logo.png" alt="Logomarca1"
-            width={200}
-            height={200}/>
-            
-            <div>
-                <h4>ENDEREÇO</h4>
-                <p>
-                    Av. Borges de Medeiros, 250 Porto Alegre, RS 90020-021
-                </p>
-            
-            </div>
+export default function Rodape() {
+  return (
+    <footer className={estilos.container_rodape}>
+      
+      <div className={estilos.container_informacoes}>
+        
+        <Image
+          className={estilos.img}
+          src="/logo.png"
+          alt="Logomarca"
+          width={100}
+          height={100}
+        />
 
-            <div>
-                <h4>CONTATO</h4>
-                <p>contato@gelateria.com.br</p>
-                <p>Tel / WhatsApp: (51) 3456-7890</p>        
-            </div>
-
-            <div>
-                <h4>HORÁRIOS</h4>
-                <p>ABERTO TODOS OS DIAS</p>
-                <p>10:00 – 22:00</p>        
-            </div>    
-        </div>
         <div>
-            <p>Gelateria. Orgulhosamente desenvolvido por Mykhael</p>
+          <h4>ENDEREÇO</h4>
+          <p>Av. Borges de Medeiros, 250</p>
+          <p>Porto Alegre, RS 90020-021</p>
         </div>
-        </section>
-        </footer>
-    );
+
+        <div>
+          <h4>CONTATO</h4>
+          <p>contato@gelateria.com.br</p>
+          <p>(51) 3456-7890</p>
+        </div>
+
+        <div>
+          <h4>HORÁRIOS</h4>
+          <p>Aberto todos os dias</p>
+          <p>10:00 – 22:00</p>
+        </div>
+
+      </div>
+
+      <div className={estilos.container_direito_autoral}>
+        <p>Gelateria. Orgulhosamente desenvolvido por Mykhael</p>
+      </div>
+
+    </footer>
+  );
 }
