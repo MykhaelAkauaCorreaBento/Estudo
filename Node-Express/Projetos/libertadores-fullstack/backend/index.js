b025fa9bc778510e45442673f2f3706c53ab75d4
MykhaelAkauaCorreaBento <mykhael.akaua.correa.bento@gmail.com>
2026-02-15 12:50:02 -0300

    Estudos

--- Node-Express/Projetos/libertadores-fullstack/backend/index.js
+++ Node-Express/Projetos/libertadores-fullstack/backend/index.js
@@ -1,5 +1,6 @@
+import { retornaCampeonatos, retornaCampeonatosAno, retornaCampeonatosID, retornaCampeonatosTime } from './servico/retornaCampeonatos_servico.js';
 import express from 'express';
-import pool from './servico/conexao.js';
+
 
 const app = express();
 
@@ -7,9 +8,39 @@ app.listen (9000, async () => {
     const data = new Date();
     console.log("Servidor node iniciado em: "+data);
 
-    const conexao = await pool.getConnection();
-
-    console.log(conexao.threadId);
-
-    conexao.release();
 })
+
+app.get('/campeonatos', async (req, res) => {
+    let campeonatos;
+
+    const ano = req.query.ano;
+    const time = req.query.time;
+
+    if(typeof ano === 'undefined' && typeof time === 'undefined') {
+        campeonatos = await retornaCampeonatos();
+    } 
+    else if (typeof ano !== 'undefined') {
+        campeonatos = await retornaCampeonatosAno(ano);
+    } 
+    else if (typeof time !== 'undefined') {
+        campeonatos = await retornaCampeonatosTime(time);
+    }
+
+    if(campeonatos.length > 0) {
+        res.json(campeonatos);
+    } else {
+        res.status(404).json({mensagem: "Nenhum campeonato encontrado"})
+    }
+});
+
+app.get('/campeonatos/:id', async (req, res) => {
+    const id = req.params.id;
+     
+    const campeonato = await retornaCampeonatosID(id);
+
+    if(campeonato.length > 0) {
+        res.json(campeonato);
+    } else {
+        res.status(404).json({mensagem: "Nenhum campeonato encontrado"})
+    }
+});
