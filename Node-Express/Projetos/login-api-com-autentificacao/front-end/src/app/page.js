470c72164487dce20e171916400e14a79c8446b0
MykhaelAkauaCorreaBento <mykhael.akaua.correa.bento@gmail.com>
2026-03-10 08:51:48 -0300

    Estudos
    Links do projeto rodando no numa aplicação fullstack(backend-api-frontend).
    
    https://drive.google.com/file/d/1wPyabezh6LPKxLKRfT7qbZ7I6Itjqiwb/view?usp=drive_link
    
    https://drive.google.com/file/d/1dqSIB0dOnBEiZrZqMb030mHZJfX_XYYZ/view?usp=drive_link

--- Node-Express/Projetos/login-api-com-autentificacao/front-end/src/app/page.js
+++ Node-Express/Projetos/login-api-com-autentificacao/front-end/src/app/page.js
@@ -1,65 +1,73 @@
-import Image from "next/image";
+'use client'
+
+import { useEffect, useState } from "react";
 import styles from "./page.module.css";
+import axios from "axios";
 
 export default function Home() {
+
+  const [leads, setLeads] = useState([]);
+
+  useEffect(() => {
+
+    const token = localStorage.getItem("token");
+
+    if(token === null) {
+      location.href = '/login';
+      return;
+    }
+
+    const listaLeads = async() => {
+      try {
+        const resposta = await axios.get('http://localhost:9000/lista-leads', {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        });
+
+        setLeads(resposta.data.listaLeads);
+      } catch(erro) {
+        if(erro.status === 401) {
+          alert("401 - Acesso não autorizado");
+          return;
+        }
+      }
+    }
+
+    listaLeads();
+   
+  }, []);
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.js file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+        <div className={styles.Usuario}>
+          <span>Olá, Administrador</span>
         </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+        <h1>Dashboard - Leads</h1>
+
+        <table border={1}>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Nome</th>
+              <th>Email</th>
+              <th>Telefone</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              leads.map((lead, index) => (
+                <tr key={index}>
+                  <td>{lead.id}</td>
+                  <td>{lead.nome}</td>
+                  <td>{lead.email}</td>
+                  <td>{lead.telefone}</td>
+                </tr> 
+              ))
+            }
+          </tbody>
+        </table>
       </main>
     </div>
   );
